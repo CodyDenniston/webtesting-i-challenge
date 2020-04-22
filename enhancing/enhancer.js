1edaad7b2b5fdc6c28e170cbbbc22e2dbc1ec111
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-04-22 15:30:33 -0500

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,55 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get,
 };
 
+// returns item modified according to rules defined by client for success
+// - enhancement level +1
+// - enhancement level 20 ? don't change
+// - durability doesn't change
 function succeed(item) {
-  return { ...item };
+	if (item.enhancement < 20) {
+		item = {
+			...item,
+			enhancement: item.enhancement + 1,
+		};
+	}
+	return item;
 }
+
+// returns item modified according to rules defined by client for failure
+// - If item's enhancement is < 15, durability is decreased by 5.
+// - If item's enhancement is > 15, durability is decreased by 10.
+// - If item's enhancement level is > 16, -1 enhancment level (17 goes down to 16, 18 goes down to 17).
 
 function fail(item) {
-  return { ...item };
+	if (item.enhancement < 15) {
+		item = {
+			...item,
+			durability: item.durability - 5,
+		};
+	} else if (item.enhancement >= 15) {
+		item = {
+			...item,
+			durability: item.durability - 10,
+		};
+	}
+	if (item.enhancement > 16) {
+		item = {
+			...item,
+			enhancement: item.enhancement - 1,
+		};
+	}
+	return item;
 }
 
+// takes item and returns an item with durability 100
 function repair(item) {
-  return { ...item };
+	return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,34 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('enhancer.js', () => {
+	it('should run tests without errors', () => {
+		// arrange --> setup the world
+		// act --> execute the code we're testing
+		// assert --> check our assumptions
+
+		expect(true).toBe(true);
+	});
+
+	describe('.repair()', function () {
+		it('Should take durability to 100', function () {
+			expect(enhancer.repair({ durability: 20 })).toEqual({ durability: 100 });
+		});
+	});
+	describe('.succeed()', function () {
+		it('should enhance the object', function () {
+			expect(enhancer.succeed({ enhancement: 5 })).toEqual({ enhancement: 6 });
+		});
+	});
+	describe('.fail()', function () {
+		it('should change durability and enhancement', function () {
+			expect(enhancer.fail({ enhancement: 5, durability: 20 })).toEqual({
+				enhancement: 5,
+				durability: 15,
+			});
+			expect(enhancer.fail({ enhancement: 20, durability: 50 })).toEqual({
+				enhancement: 19,
+				durability: 40,
+			});
+		});
+	});
+});
